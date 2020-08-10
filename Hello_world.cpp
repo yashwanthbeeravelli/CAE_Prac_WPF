80eb08ca356d9a71499680fbe7d3f983d2068973
Yashwanth Beeravelli <yashwanthbeeravelli@gmail.com>
2020-08-10 14:37:01 -0400

    Added the first file called "HellowWorld.cpp"

---
+++ Hello_world.cpp
@@ -0,0 +1,22 @@
+// Your First C++ Program
+
+#include <iostream> //Header File //Libraries
+
+int main() 
+{ 
+    int a;
+    a = 100;
+    std::string c = "Hello";
+	std::cout << c;
+    std::cout << a;
+    return 1;
+    //addition();
+}
+
+
+/*
+addition()
+{
+	cout << "Hello world";
+}
+*/
