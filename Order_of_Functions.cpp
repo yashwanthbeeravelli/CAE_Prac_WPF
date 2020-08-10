f16bb772f5ffaedfc1c395c86318d2201656d139
Yashwanth Beeravelli <yashwanthbeeravelli@gmail.com>
2020-08-10 15:01:39 -0400

    Adding the third file called Order_Of_Functions.cpp

---
+++ Order_of_Functions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void doPrint()
+{
+	std::cout << "I am inside doPrint function";
+}
+
+int main ()
+{
+	std:: cout << "This is the first function that's is going to be executed.";
+	doPrint();
+	std::cout << "I will print after the doPrint()";
+	return 1;
+}
+
