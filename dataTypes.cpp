50c557a91a608023956cc7c6210b0bfacace04bf
Yashwanth Beeravelli <yashwanthbeeravelli@gmail.com>
2020-08-10 14:37:36 -0400

    Adding the second file called dataTypes.cpp

---
+++ dataTypes.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+//using namespace std;
+//#include<basic_string>
+ int main() 
+{
+  float myNum = 5.75;
+  double myNum1 = 19.99;
+  float f1 = 35e3;
+  double d1 = 12E4;   
+  bool isCodingFun = true;
+  char myGrade = 'B';
+  std::string greeting = "Hello";
+  std::cout << myNum<<"\n";
+  std::cout<<myNum1<<"\n";
+  std::cout<<f1<<"\n";
+  std::cout<<d1<<"\n";
+  std::cout<<isCodingFun<<"\n";
+  std::cout<<myGrade<<"\n";
+  std::cout<<greeting<<"\n";
+  return 0;
+}
+
